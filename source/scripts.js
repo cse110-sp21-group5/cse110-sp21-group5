81a119be858890d27d2e2bbf1245dd7bc596453c
Evan Chen <evchen@ucsd.edu>
2021-05-18 23:50:00 -0700

    found way to get months

--- source/scripts.js
+++ source/scripts.js
@@ -1,48 +1,45 @@
 // This is where we will write our JS
 
-//select the <a> tags used for navigation at the top of the page
-let dispBar = document.querySelectorAll(".nav a");
-let newEntry = document.querySelector('[class=addEntry]');
-let form = document.createElement('form');
-let textArea = document.createElement('textarea');
-let submit = document.createElement('input');
-submit.setAttribute('type','submit');
+// select the <a> tags used for navigation at the top of the page
+const dispBar = document.querySelectorAll('.nav a');
+const newEntry = document.querySelector('[class=addEntry]');
+const form = document.createElement('form');
+const textArea = document.createElement('textarea');
+const submit = document.createElement('input');
+submit.setAttribute('type', 'submit');
 form.append(submit);
 form.append(textArea);
-textArea.setAttribute('rows',30);
-textArea.setAttribute('cols',100);
+textArea.setAttribute('rows', 30);
+textArea.setAttribute('cols', 100);
 
 /**
  * Clears the active <a> tag
  */
-function clrActive() {
-    for (let i = 0; i < dispBar.length; i++) {
-        dispBar[i].classList.remove("active");
-    }
+function clrActive () {
+  for (let i = 0; i < dispBar.length; i++) {
+    dispBar[i].classList.remove('active');
+  }
 }
 
 /**
  * Adds event listeners to all elements of the navigation bar selected in dispBar
  */
 for (let i = 0; i < dispBar.length; i++) {
-    
-    dispBar[i].addEventListener('click', function() {
-        
-        clrActive();
-        
-        dispBar[i].classList.add("active");
-    });
+  dispBar[i].addEventListener('click', function () {
+    clrActive();
+
+    dispBar[i].classList.add('active');
+  });
 }
 
 /**
  * Appends the created form to 'main' when "New Entry" is clicked
  */
 newEntry.addEventListener('click', () => {
-    document.querySelector('main').append(form);
-  });
+  document.querySelector('main').append(form);
+});
 
-
-submit.addEventListener('click',() => {
+submit.addEventListener('click', () => {
 
 });
 
@@ -65,3 +62,20 @@ document.addEventListener('DOMContentLoaded', () => {
       console.log(`%cresult of fetch is an error: \n"${error}"`, 'color: red');
     });
 });
+
+/*
+ * Begin process of laying out months
+ */
+const allMonths = { 0: 'Jan', 1: 'Feb', 2: 'Mar', 3: 'Apr', 4: 'May', 5: 'Jun', 6: 'Jul', 7: 'Aug', 8: 'Sep', 9: 'Oct', 10: 'Nov', 11: 'Dec' };
+
+let date = new Date();
+
+let month = date.getMonth();
+
+for (let i = 0; i < 12; i++) {
+  let textMonth = document.createElement('p');
+  textMonth.innerText = allMonths[month];
+  document.querySelector('aside').appendChild(textMonth);
+  
+  month = (month + 1) % 12;
+}
