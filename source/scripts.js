4cd045609bee2173561597ed5d2e34f1123d0959
ntaherni <ntaherni@ucsd.edu>
2021-05-22 18:33:04 -0700

    filtering code added

--- source/scripts.js
+++ source/scripts.js
@@ -1,49 +1,84 @@
 // This is where we will write our JS
 
-//select the <a> tags used for navigation at the top of the page
-let dispBar = document.querySelectorAll(".nav a");
-let newEntry = document.querySelector('[class=addEntry]');
-let form = document.createElement('form');
-let textArea = document.createElement('textarea');
-let submit = document.createElement('input');
-submit.setAttribute('type','submit');
+// select the <a> tags used for navigation at the top of the page
+const dispBar = document.querySelectorAll('.nav a');
+const newEntry = document.querySelector('[class=addEntry]');
+const form = document.createElement('form');
+const textArea = document.createElement('textarea');
+const submit = document.createElement('input');
+const filter = document.querySelector('[name="filter"]');
+submit.setAttribute('type', 'submit');
 form.append(submit);
 form.append(textArea);
-textArea.setAttribute('rows',30);
-textArea.setAttribute('cols',100);
+textArea.setAttribute('rows', 30);
+textArea.setAttribute('cols', 100);
 
 /**
  * Clears the active <a> tag
  */
-function clrActive() {
-    for (let i = 0; i < dispBar.length; i++) {
-        dispBar[i].classList.remove("active");
-    }
+function clrActive () {
+  for (let i = 0; i < dispBar.length; i++) {
+    dispBar[i].classList.remove('active');
+  }
 }
 
 /**
  * Adds event listeners to all elements of the navigation bar selected in dispBar
  */
 for (let i = 0; i < dispBar.length; i++) {
-    
-    dispBar[i].addEventListener('click', function() {
-        
-        clrActive();
-        
-        dispBar[i].classList.add("active");
-    });
+  dispBar[i].addEventListener('click', function () {
+    clrActive();
+    dispBar[i].classList.add('active');
+  });
 }
 
 /**
  * Appends the created form to 'main' when "New Entry" is clicked
  */
 newEntry.addEventListener('click', () => {
-    document.querySelector('main').append(form);
-  });
+  document.querySelector('main').append(form);
+});
 
+submit.addEventListener('click', () => {
 
-submit.addEventListener('click',() => {
+});
 
+/**
+ * Changes the value of the filter based on the option selected
+ * Changes the journal entries displayed based on the filter selection
+ */
+filter.addEventListener('change', () => {
+  const url = './sample-entries.json';
+  document.querySelectorAll('journal-entry').forEach(e => e.remove());
+  fetch(url)
+    .then(entries => entries.json())
+    .then(entries => {
+      entries.sort((a, b) => Date.parse(b.date) - Date.parse(a.date));
+      if (filter.value !== 'date') {
+        const filtered = entries.filter(function (entry) {
+          for (let i = 0; i < entry.tags.length; i++) {
+            if (entry.tags[i] === filter.value) {
+              return entry;
+            }
+          }
+          return null;
+        });
+        filtered.forEach((entry) => {
+          console.log(entry);
+          const newPost = document.createElement('journal-entry');
+          newPost.entry = entry;
+        });
+      } else {
+        entries.forEach((entry) => {
+          console.log(entry);
+          const newPost = document.createElement('journal-entry');
+          newPost.entry = entry;
+        });
+      }
+    })
+    .catch(error => {
+      console.log(`%cresult of fetch is an error: \n"${error}"`, 'color: red');
+    });
 });
 
 /**
@@ -55,13 +90,34 @@ document.addEventListener('DOMContentLoaded', () => {
   fetch(url)
     .then(entries => entries.json())
     .then(entries => {
+      entries.sort((a, b) => Date.parse(b.date) - Date.parse(a.date));
       entries.forEach((entry) => {
         console.log(entry);
         const newPost = document.createElement('journal-entry');
         newPost.entry = entry;
+        const tags = entry.tags;
+        const existingOptions = [];
+        for (const option of filter.options) {
+          existingOptions.push(option.value);
+        }
+        for (let i = 0; i < tags.length; i++) {
+          const opt = document.createElement('option');
+          opt.value = tags[i];
+          opt.innerHTML = capitalizeFirstLetter(tags[i]);
+          if (!existingOptions.includes(opt.value)) {
+            filter.appendChild(opt);
+          }
+        }
       });
     })
     .catch(error => {
       console.log(`%cresult of fetch is an error: \n"${error}"`, 'color: red');
     });
 });
+
+/**
+ * Function to capitalize first letter in a string (for tags)
+ */
+function capitalizeFirstLetter (str) {
+  return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+}
