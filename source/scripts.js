535bcc3bf891d1915101bd9c30a6fecc51cf579e
jchung01 <jjchung276@gmail.com>
2021-05-30 21:59:42 -0700

    initial commit w/ delete mode

--- source/scripts.js
+++ source/scripts.js
@@ -1,19 +1,43 @@
 // This is where we will write our JS
-// Checked out from branch "filter" on 5/23 8:40 PM PST
 
 // select the <a> tags used for navigation at the top of the page
 const dispBar = document.querySelectorAll('.nav a');
 const newEntry = document.querySelector('[class=addEntry]');
-let currentEntries;
 const form = document.createElement('form');
 const textArea = document.createElement('textarea');
-const submit = document.createElement('input');
-const filter = document.querySelector('[name="filter"]');
-submit.setAttribute('type', 'submit');
-form.append(submit);
+let existingEntry = false;
+let deleteMode = false;
 form.append(textArea);
-textArea.setAttribute('rows', 30);
-textArea.setAttribute('cols', 100);
+textArea.setAttribute('rows', 3);
+textArea.setAttribute('cols', 50);
+
+const request = indexedDB.open('Daily', 1);
+let db;
+request.onerror = function (event) {
+  console.log('Error opening IndexedDB');
+};
+
+request.onsuccess = function (event) {
+  db = event.target.result;
+  getAndShowEntries(db);
+};
+
+request.onerror = function (event) {
+  console.error('IndexedDB erorr: ' + event.target.errorCode);
+};
+
+request.onupgradeneeded = function (event) {
+  db = event.target.result;
+  /*
+  if (document.querySelector('h3') !== null) {
+    let date = document.querySelector('h3').innerHTML;
+    if(!db.objectStoreNames.contains(date)) {
+      db.createObjectStore(date, { autoIncrement: true});
+    }
+  }
+  */
+  const entries = db.createObjectStore('entries', { autoIncrement: true });
+};
 
 /**
  * Clears the active <a> tag
@@ -25,6 +49,7 @@ function clrActive () {
 }
 
 /**
+ * @function
  * Adds event listeners to all elements of the navigation bar selected in dispBar
  */
 for (let i = 0; i < dispBar.length; i++) {
@@ -36,108 +61,329 @@ for (let i = 0; i < dispBar.length; i++) {
 
 /**
  * Appends the created form to 'main' when "New Entry" is clicked
+ * @function
  */
+
 newEntry.addEventListener('click', () => {
-  document.querySelector('main').append(form);
-});
-
-submit.addEventListener('click', () => {
-
+  if (document.querySelector('section') === null) {
+    document.querySelector('main').append(form);
+  } else {
+    document.querySelector('section').append(form);
+  }
 });
 
 /**
- * Changes the value of the filter based on the option selected
- * Changes the journal entries displayed based on the filter selection
+ * @function
+ * Make a new bullet when enter is pressed (rather than newline)
  */
-filter.addEventListener('change', () => {
-  const url = './sample-entries.json';
-  document.querySelectorAll('journal-entry').forEach(e => e.remove());
-  fetch(url)
-    .then(entries => entries.json())
-    .then(entries => {
-      entries.sort((a, b) => Date.parse(b.date) - Date.parse(a.date));
-      if (filter.value !== 'date') {
-        const filtered = entries.filter(function (entry) {
-          for (let i = 0; i < entry.tags.length; i++) {
-            if (entry.tags[i] === filter.value) {
-              return entry;
-            }
-          }
-          return null;
-        });
-        filtered.forEach((entry) => {
-          console.log(entry);
-          const newPost = document.createElement('journal-entry');
-          newPost.entry = entry;
-        });
-      } else {
-        entries.forEach((entry) => {
-          console.log(entry);
-          const newPost = document.createElement('journal-entry');
-          newPost.entry = entry;
-        });
-      }
-    })
-    .catch(error => {
-      console.log(`%cresult of fetch is an error: \n"${error}"`, 'color: red');
-    });
+textArea.addEventListener('keyup', function (event) {
+  if (event.keyCode === 13) {
+    addEntry();
+  }
 });
 
 /**
  * Loads entries and renders them to index.html
  */
+/*
 document.addEventListener('DOMContentLoaded', () => {
-  const url = './sample-entries.json'; // SET URL
-
+  let url = "./sample-entries.json"  // SET URL
   fetch(url)
     .then(entries => entries.json())
     .then(entries => {
-      entries.sort((a, b) => Date.parse(b.date) - Date.parse(a.date));
       entries.forEach((entry) => {
         console.log(entry);
-        const newPost = document.createElement('journal-entry');
+        let newPost;
+
+        newPost = document.createElement('journal-entry');
         newPost.entry = entry;
-        const tags = entry.tags;
-        const existingOptions = [];
-        for (const option of filter.options) {
-          existingOptions.push(option.value);
-        }
-        for (let i = 0; i < tags.length; i++) {
-          const opt = document.createElement('option');
-          opt.value = tags[i];
-          opt.innerHTML = capitalizeFirstLetter(tags[i]);
-          if (!existingOptions.includes(opt.value)) {
-            filter.appendChild(opt);
-          }
-        }
       });
-    })
-    .then(() => {
-      addFlags();
     })
     .catch(error => {
       console.log(`%cresult of fetch is an error: \n"${error}"`, 'color: red');
     });
 });
+*/
 
 /**
- * Function to capitalize first letter in a string (for tags)
+ * Adds a new bullet to the current date and stores the data in the database (IndexedDB)
  */
-function capitalizeFirstLetter (str) {
-  return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+function addEntry () {
+  // Checks to see if the input is empty
+  if (textArea.value.length === 1) {
+    document.querySelector('form').remove();
+    textArea.value = '';
+    return;
+  }
+  // let date1 = new Date('2021', '5', '25');
+  const date = new Date().toLocaleDateString();
+  const entryDiv = document.createElement('div');
+  entryDiv.className = date;
+  const newEntry = document.createElement('li');
+  // const deleteButton = document.createElement('button');
+  // deleteButton.className = 'delete';
+  // entryDiv.append(deleteButton);
+  // create flag button
+  const flagButton = document.createElement('button');
+  flagButton.className = 'flag';
+  flagButton.setAttribute('state', 0);
+  entryDiv.append(flagButton);
+  let dateExists = false;
+  const sectionList = document.querySelectorAll('section');
+  let sectionExists = false;
+  let section;
+  if (sectionList !== null) {
+    sectionList.forEach(sec => {
+      if (sec.className === date) {
+        sectionExists = true;
+        section = sec;
+      }
+    });
+  }
+  if (sectionExists === false) {
+    section = document.createElement('section');
+    section.className = date;
+  }
+  // Adds date on the first entry of the day
+  if (document.querySelector('h3') === null) {
+    const newEntryTitle = document.createElement('h3');
+    newEntryTitle.innerText = date;
+    section.append(newEntryTitle);
+    document.querySelector('main').append(section);
+    addEntrytoDB(db, newEntryTitle, newEntryTitle.innerText);
+  } else {
+    const h3List = document.querySelectorAll('h3');
+    h3List.forEach(h3 => {
+      if (h3.innerText === date) {
+        dateExists = true;
+      }
+    });
+    if (dateExists === false) {
+      const newEntryTitle = document.createElement('h3');
+      newEntryTitle.innerText = date;
+      section.append(newEntryTitle);
+      document.querySelector('main').append(section);
+      addEntrytoDB(db, newEntryTitle, newEntryTitle.innerText);
+    }
+  }
+  // Adds bullets, reset text area and delete the form
+  newEntry.innerText = textArea.value;
+  entryDiv.append(newEntry);
+  section.append(entryDiv);
+  document.querySelector('form').remove();
+  textArea.value = '';
+  addEntrytoDB(db, entryDiv, date);
 }
 
 /**
- * Function to add flag button to each entry
- *
- * Currently does not work for added entries, only loaded ones.
+ * Retrieves the entries of the database and shows the entries on the screen
+ * @param  database The database that will be used to display entries
  */
-function addFlags () {
-  currentEntries = document.querySelectorAll('journal-entry');
-  currentEntries.forEach((entry) => {
-    const flag = entry.flag;
-    flag.addEventListener('click', () => {
-      // add tag "Important" to entry tags or journal property?
-    });
+function getAndShowEntries (database) {
+  const transaction = database.transaction(['entries'], 'readonly');
+  const objStore = transaction.objectStore('entries');
+  const request1 = objStore.openCursor();
+  const entries = [];
+  request1.onsuccess = function (event) {
+    const cursor = event.target.result;
+    if (cursor !== null) {
+      console.log(cursor.value);
+      entries.push(cursor.value);
+      console.log(entries.length);
+      cursor.continue();
+    } else {
+      showEntries(entries);
+    }
+  };
+}
+
+/**
+ * Adds the given entry to the database
+ * @param  database The database that entries will be added to
+ * @param {Object} entry The entry that will be added to the database
+ * @param {string} day The day the entry was made
+ */
+function addEntrytoDB (database, entry, day) {
+  const transaction = database.transaction(['entries'], 'readwrite');
+  const objStore = transaction.objectStore('entries');
+  const entryText = entry.innerText;
+  const entryObject = { content: entryText, date: day };
+  objStore.add(entryObject);
+  transaction.oncomplete = function () {
+    console.log('Data entered into database');
+  };
+  transaction.onerror = function (event) {
+    console.log('Error entering data into database' + event.target.errorCode);
+  };
+}
+
+/**
+ * Shows the given entries on the screen
+ * Helper function for getAndShowEntries()
+ * @param {Object[]} entries The list of entries that will be shown on the screen
+ */
+function showEntries (entries) {
+  entries.forEach((entry) => {
+    if (textArea.value.length === 1) {
+      document.querySelector('form').remove();
+      textArea.value = '';
+      return;
+    }
+    const entryDiv = document.createElement('div');
+    // const deleteButton = document.createElement('button');
+    // deleteButton.className = 'delete';
+    // entryDiv.append(deleteButton);
+    // create flag button
+    const flagButton = document.createElement('button');
+    flagButton.className = 'flag';
+    entryDiv.append(flagButton);
+    entryDiv.className = entry.date;
+    const newEntry = document.createElement('li');
+    const sectionList = document.querySelectorAll('section');
+    let section;
+    let sectionExists = false;
+    if (sectionList !== null) {
+      sectionList.forEach(sec => {
+        if (sec.className === entry.date) {
+          sectionExists = true;
+          section = sec;
+        }
+      });
+    }
+    if (sectionExists === false) {
+      section = document.createElement('section');
+      section.className = entry.date;
+      const sec = document.querySelector('section');
+      if (sec === null) {
+        document.querySelector('main').append(section);
+      } else {
+        document.querySelector('main').insertBefore(section, sec);
+      }
+    }
+    if (entry.content === entry.date) {
+      const dateTitle = document.createElement('h3');
+      dateTitle.innerText = entry.date;
+      section.append(dateTitle);
+      return;
+    }
+    // Adds bullets, reset text area and delete the form
+    newEntry.innerText = entry.content;
+    entryDiv.append(newEntry);
+    section.append(entryDiv);
+    textArea.value = '';
   });
+}
+
+/**
+ * Deletes the selected event when in delete mode
+ * Helper function for use on clicking entry
+ * @param event The event caused by clicking the entry
+ */
+function removeEntry (event) {
+  // Get the div element
+  const divElement = event.target.parentNode;
+  const content = divElement.innerText;
+  const date = divElement.className;
+  // Remove element from IndexedDB
+  const transaction = db.transaction(['entries'], 'readwrite');
+  const objStore = transaction.objectStore('entries');
+  const request1 = objStore.openCursor();
+  request1.onsuccess = function (event) {
+    const cursor = event.target.result;
+    if (cursor === null) {
+      return;
+    }
+    if (cursor.value.content === content && cursor.value.date === date) {
+      objStore.delete(cursor.key); // Delete appropriate element from DB
+      divElement.remove(); // Delete div element from page
+      return;
+    }
+    cursor.continue();
+  };
+  request1.onerror = function (event) {
+    console.error('IndexedDB erorr: ' + event.target.errorCode);
+  };
+}
+
+/**
+ * Checks to see if delete button clicked. If so, enables delete mode.
+ * Otherwise checks to see if an entry was clicked. If so allows deleting or editing entry, depending on mode.
+ * @function
+ */
+document.addEventListener('click', function (event) {
+  const divElement = event.target.parentNode;
+  const day = divElement.className;
+  let oldContent;
+  if (divElement.querySelector('li') !== null) {
+    oldContent = divElement.querySelector('li').innerText;
+  }
+  // toggle delete mode
+  if (event.target.className === 'deleteEntry') {
+    const delText = document.getElementById('deleteText');
+    if (deleteMode === true) {
+      deleteMode = false;
+      delText.style.visibility = 'hidden';
+    } else {
+      deleteMode = true;
+      delText.style.visibility = 'visible';
+    }
+  } else if (event.target.className === 'flag') {
+    // show/hide flagged for each entry
+    const flag = event.target.parentNode.querySelector('.flag');
+    console.log(flag.getAttribute('state'));
+    if (flag.getAttribute('state') === 0) {
+      flag.setAttribute('state', 1);
+    } else if (flag.getAttribute('state') === 1) {
+      flag.setAttribute('state', 0);
+    }
+  } else if (divElement.tagName === 'DIV') {
+    // delete if delete mode, otherwise edit entry
+    if (deleteMode) {
+      removeEntry(event);
+    } else if (existingEntry === false) {
+      existingEntry = true;
+      const textBox = document.createElement('textarea');
+      if (divElement.querySelector('li') !== null) {
+        textBox.innerText = divElement.querySelector('li').innerText;
+        divElement.querySelector('li').remove();
+        divElement.append(textBox);
+      }
+      const editedEntry = document.createElement('li');
+      textBox.addEventListener('keyup', function (event) {
+        if (event.keyCode === 13) {
+          editedEntry.innerText = textBox.value;
+          textBox.remove();
+          divElement.append(editedEntry);
+          existingEntry = false;
+          // Update appropriate entry in DB
+          updateDB(editedEntry.innerText, oldContent, day);
+        }
+      });
+    }
+  }
+});
+
+/**
+ * Updates the database, replacing oldContent with entry
+ * @param {string} entry The new content of the entry
+ * @param {string} oldContent The old content of the entry that will be replaced by "entry"
+ * @param {string} day The date of the entry
+ */
+function updateDB (entry, oldContent, day) {
+  const bullet = entry;
+  const transaction = db.transaction(['entries'], 'readwrite');
+  const objStore = transaction.objectStore('entries');
+  const request1 = objStore.openCursor();
+  const newContent = { content: bullet, date: day };
+  request1.onsuccess = function (event) {
+    const cursor = event.target.result;
+    if (cursor === null) {
+      return;
+    }
+    if (cursor.value.content === oldContent && cursor.value.date === day) {
+      objStore.put(newContent, cursor.key); // Update appropriate element from DB
+      return;
+    }
+    cursor.continue();
+  };
 }
